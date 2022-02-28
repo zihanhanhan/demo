aad8dd42d1b9dbb327683f079f96afb1c9cbaa8a
zihanhanhan <835937922@qq.com>
2022-02-28 13:04:27 +0800

    second3

--- demo.py
+++ demo.py
@@ -1,5 +1,39 @@
-print("cuixing")
-print("zihan")
-print("love")
-print("dada")
-print
+import multiprocessing
+import os
+import sys
+import threading
+from time import sleep
+
+
+def thread_demo(index):
+    while True:
+        print(f"thread_demo {index} pid={os.getpid()} thread_name={threading.current_thread().name}")
+        sleep(1)
+
+
+def process_demo(index):
+    while True:
+        print(f"process_demo {index} pid={os.getpid()} thread_name={threading.current_thread().name}")
+        sleep(1)
+
+
+def multi_thread(count):
+    for index in range(count):
+        thread = threading.Thread(target=thread_demo, args=[index])
+        thread.start()
+        sleep(1)
+
+
+def multi_process(count):
+    for index in range(count):
+        process = multiprocessing.Process(target=process_demo, args=(index,))
+        process.start()
+        sleep(1)
+
+
+if __name__ == '__main__':
+    process_count = int(sys.argv[1])
+    thread_count = int(sys.argv[2])
+    multi_process(process_count)
+    multi_thread(thread_count)
+    input()
